24a4ae49c99e00238c008e3e974238f8057f267d
Nehuel <111355407+Nehuelin@users.noreply.github.com>
2026-07-11 17:13:17 -0300

    feat: initialize carts router

---
+++ src/routes/carts.router.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Carts router is working",
+    payload: [],
+  });
+});
+
+module.exports = router;
